d96b441bfe5221e66023bc0b01a7df547ece0f74
mailtomeforinfo <mailtomeforinfo@gmail.com>
2023-10-29 19:23:10 +0530

    code new16

--- scripts/copy_custom_index.sh
+++ scripts/copy_custom_index.sh
@@ -7,6 +7,7 @@ custom_index_file="scripts/index.html"
 nginx_html_dir="/usr/share/nginx/html/"
 
 # Copy the custom index.html to the Nginx directory
-sudo cp "$custom_index_file" "$nginx_html_dir"
+##sudo cp -av "$custom_index_file" "$nginx_html_dir"
+sudo cp scripts/index.html /usr/share/nginx/html/
 echo "Script executed successfully" >> /tmp/deployment.log
 
